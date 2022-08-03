cb01fd7036ecb7fcaa9f16c389b3ef3f9dc8c389
martuzads <102385507+martuzads@users.noreply.github.com>
2022-08-03 20:01:55 +0300

    Feature 3 (#3)
    * first commit: model User + raw signup api-view (without confirmation_code), no users/ endpoints etc
    
    * Добавлены: модели(Review, Comments), Админка(ReviewAdmin,CommentAdmin),Сериализаторы(ReviewSerializer,CommentSerializer),Вьюсеты(ReviewViewSet,CommentViewSet),Роутер на эндпоинты(reviews, comments)

--- api_yamdb/api/serilizers.py
+++ api_yamdb/api/serilizers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError, NotFound
 from rest_framework_simplejwt.tokens import AccessToken
 
-from reviews.models import User
+from reviews.models import User, Review, Comments
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -140,3 +140,54 @@ class YAMDbTokenObtainSerializer(serializers.Serializer):
         data["access"] = str(self.token.for_user(user))
 
         return data
+
+
+# Часть третьего разработчика (Дмитрий, ветка feature_3)
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализация отзывов."""
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        # на счёт параметра default я плохо понял
+        # если пайтест не пройдёт, то попробовать
+        # его разкомментить
+        # default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=['title', 'author'],
+                message='Отзыв на произведение уже есть'
+            )
+        ]
+
+    def validate(self, data):
+        if data['title'] == data['author']:
+            raise serializers.ValidationError(
+                'Повторно оставить отзыв на произведение невозможно'
+            )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализация комментариев."""
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comments
+        fields = '__all__'
