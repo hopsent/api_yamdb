42b33da86e8ff844f31eee927c4a2a1b33e40263
Aleksei <102385357+hopsent@users.noreply.github.com>
2022-08-07 19:57:58 +0300

    hopefully last one

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -247,6 +247,6 @@ class TitleCreateSerializer(serializers.ModelSerializer):
             if year > datetime.date.today().year:
                 message = 'Год выпуска не может быть больше текущего'
                 raise serializers.ValidationError(message)
-        except KeyError and ValidationError:
+        except KeyError or ValidationError:
             raise KeyError('Необходимо указать год')
         return data
